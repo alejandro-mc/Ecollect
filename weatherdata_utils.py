560016ee1bef87d1900b662a8b74ed221b055134
alejandro-mc <morejoncortina@gmail.com>
2017-06-14 17:40:30 -0400

    Adds class to read data from noaa weather station feed

---
+++ weatherdata_utils.py
@@ -0,0 +1,43 @@
+import urllib.request as http
+import xml.etree.ElementTree as ET
+
+
+class WeatherStation:
+    '''Handles connection to weather station feed
+    and extraction of temperature,pressure and humidity from raw response'''
+    def __init__(self,url):
+        self.__temp__     = None
+        self.__pressure__ = None
+        self.__humidity__ = None
+        self.__url__      = url
+        self.raw=""
+    
+    def update(self):
+        #get raw data from url
+        rawtxt = http.urlopen(self.__url__).read()
+        self.raw = rawtxt
+        
+        #parse xml to get data
+        (self.__temp__,
+        self.__pressure__,
+        self.__humidity__) = self.__parse__(rawtxt)
+    
+    def __parse__(self,raw):
+        tree = ET.fromstring(raw)
+        
+        str_temp_f            = [i for i in tree.find('temp_f').itertext()     ][0]
+        str_pressure_mb       = [i for i in tree.find('pressure_mb').itertext()][0]
+        str_relative_humidity = [i for i in tree.find('relative_humidity').itertext()][0]
+        
+        return (float(str_temp_f           ),
+                float(str_pressure_mb      ),
+                float(str_relative_humidity))
+    
+    def temp(self):
+        return self.__temp__
+    
+    def pressure(self):
+        return self.__pressure__
+    
+    def humidity(self):
+        return self.__humidity__
